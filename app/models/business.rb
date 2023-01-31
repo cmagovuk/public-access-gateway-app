1be9f959461a2e527e79f49c6b11d8ca3ca53d0f
Jonathan Beckett <jonathan.beckett@gmail.com>
2023-01-31 14:55:11 +0000

    Added postcode format validation to the business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,11 +2,15 @@ class Business < ApplicationRecord
 
     belongs_to :submission
     
+    POSTCODE_REGEX = /\A([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})\z/.freeze
+    
     validates :name, presence:true, length: { maximum: 255 }
     validates :street, length: { maximum: 255 }
     validates :townorcity, length: { maximum: 255 }
     validates :county, length: { maximum: 50 }
-    validates :postcode, length: { maximum: 10 }
+    validates :postcode, length: { maximum: 10 }, format: { with: POSTCODE_REGEX }, allow_blank: true
     validates :url, length: { maximum: 255 }
+    
+
     
 end
